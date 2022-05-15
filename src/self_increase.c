8597c718d90761a58400ac04cbd16f000fba1249
xiangjianghai <xtc11002200@outlook.com>
2022-05-15 14:10:20 +0800

    add self_increase

---
+++ src/self_increase.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    #if 0 //自增运算符
+    int i = 1;
+    // int j = 2;
+    printf("%d\n", (++i)*(++i));
+    // printf("%d\n", i+++j);
+    #else
+    int i;
+    int a[0];
+    printf("hello world!\n");
+    int j;
+    for (int k=0; k<10 ; k++);
+    #endif
+
+    return 0;
+}
